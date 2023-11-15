cfb526638eb40380701c32c53dde97fc4e661077
NickZane <nickzane@users.noreply.github.com>
2023-11-15 14:30:26 -0800

    Very No
    Very No

--- generator/js/functions.js
+++ generator/js/functions.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const darkModeToggle = document.getElementById('darkModeToggle');
   const body = document.body;
   darkModeToggle.addEventListener('change', function () {
-    body.classList.toggle('dark-mode', darkModeToggle.checked);
+    body.classList.toggle('dark-mode', darkModeToggle.unchecked);
   });
 });
 
